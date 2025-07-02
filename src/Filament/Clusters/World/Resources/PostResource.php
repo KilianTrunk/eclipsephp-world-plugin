333eb81fe2f2e4f9056be30b2da56608e8cae579
SlimDeluxe <131700+SlimDeluxe@users.noreply.github.com>
2025-07-02 13:48:35 +0000

    style: fix code style

--- src/Filament/Clusters/World/Resources/PostResource.php
+++ src/Filament/Clusters/World/Resources/PostResource.php
@@ -23,7 +23,7 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class PostResource extends Resource  implements HasShieldPermissions
+class PostResource extends Resource implements HasShieldPermissions
 {
     protected static ?string $model = Post::class;
 
