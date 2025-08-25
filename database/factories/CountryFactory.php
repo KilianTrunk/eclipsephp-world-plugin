07942602f6a6202ecb4a7652a897f5c076d1daeb
Omer Šabić <omer@datalinx.si>
2025-08-25 09:58:23 +0200

    build: fix country factory

--- database/factories/CountryFactory.php
+++ database/factories/CountryFactory.php
@@ -22,8 +22,8 @@ class CountryFactory extends Factory
     public function definition(): array
     {
         return [
-            'id' => fake()->lexify('??'),
-            'a3_id' => fake()->lexify('???'),
+            'id' => strtoupper(fake()->lexify('??')),
+            'a3_id' => strtoupper(fake()->lexify('???')),
             'num_code' => str_pad(fake()->numberBetween(1, 999), 3, '0', STR_PAD_LEFT),
             'name' => fake()->country(),
             'flag' => fake()->emoji(),
